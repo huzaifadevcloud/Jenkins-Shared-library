ba59712725fd43fc29efeb9eb3a993951b02858a
Huzaifa Hatim <huzaifahatim2000@hotmail.com>
2025-07-21 23:15:55 +0500

    Create deploy.groovy

---
+++ vars/deploy.groovy
@@ -0,0 +1,20 @@
+def call(String fullImage) {
+    echo "Starting deployment using image: ${fullImage}"
+
+    // Run container and expose port 81015
+    sh """
+        docker run --rm -p 8105:8105 ${fullImage}
+    """
+
+    // Optional: Test route and stop container manually if needed
+    // If using `-d`, then you can test and stop it like this:
+    /*
+    sh '''
+        docker run -d --name django-notes-app -p 8105:8105 ${fullImage}
+        sleep 5
+        curl -f http://localhost:8000/demo || echo "Server did not respond"
+        docker stop django-notes-app
+        docker rm django-notes-app
+    '''
+    */
+}
